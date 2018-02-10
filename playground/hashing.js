11f07da0de7f48d2e3dcc1e6ebb4eee31ef727da
Jiaqi Duan <jiaqiduan@Jiaqis-MacBook-Pro.local>
2018-02-09 22:21:43 -0800

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,17 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+
+const data = {
+  id:10
+};
+const token = jwt.sign(data,'123abc');
+console.log(token);
+ const decoded = jwt.verify(token,'123abc');
+ console.log('decoded',decoded);
+
+// const message = 'I am user number 3';
+// const hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`)
