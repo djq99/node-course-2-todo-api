dc1c0d645af7f41fc35f9d5a94bfc9ed819677e1
Jiaqi Duan <jiaqiduan@Jiaqis-MacBook-Pro.local>
2018-02-16 18:45:44 -0800

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
-const {authenticate} = require('./middleware/authenticate');  
+const {authenticate} = require('./middleware/authenticate');
 
 
 const app = express();
@@ -120,6 +120,20 @@ app.post('/users',(req,res)=>{
 
 app.get('/users/me', authenticate,(req,res)=>{
   res.send(req.user);
+})
+
+
+//POST /users/login {email,password}
+app.post('/users/login',(req,res)=>{
+  const body = _.pick(req.body,['email','password']);
+
+  User.findByCredentials(body.email,body.password).then((user)=>{
+    user.generateAuthToken().then((token)=>{
+      res.header('x-auth',token).send(user);
+    });
+  }).catch((e)=>{
+    res.status(400).send();
+  })
 })
 
 
